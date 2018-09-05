fc5483ee091e94f1a58cee9562097e794d326d01
root <root@endusup1499.latam-eig.com>
2018-09-05 13:20:17 -0300

    novas aulas

---
+++ abstrata/config.php
@@ -0,0 +1,15 @@
+<?php
+
+spl_autoload_register(
+function ($nameClass))
+	{
+	$dirClass = "class";
+	$filename = $dirclass . DIRECTORY_SEPARATOR . $nameClass".php";
+	if (file_exists($filename))
+		{
+		require_once ($filename);
+
+		}
+	}
+
+?>
---
+++ namespace/class/Cadastro.php
@@ -0,0 +1,58 @@
+<?php
+
+class Cadastro{
+
+	private $nome;
+	private $email;
+	private $senha;
+
+    //geters
+	public function getNome()::string {
+
+
+		return $this->nome;
+
+	}
+
+	public function getEmail()::string {
+
+
+		return $this->email;
+		
+	}
+
+	public function getSenha()::string {
+
+
+		return $this->senha;
+		
+	}
+    
+
+    //seters
+
+    public function setNome($nome){
+
+         $this->nome = $nome;
+
+
+    }
+
+    public function setEmail($email){
+
+         $this->email = $email;
+
+    }
+
+    public function setSenha($senha){
+
+         $this->senha = $senha;
+
+    //tostring with json
+
+    public function __tostring(){
+
+    	return json_encode()
+    }
+
+?>
